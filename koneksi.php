cd5ebf364fe3a6ac628d0213db68fcfb0a36902e
gensza <mohamadgenszavernando@gmail.com>
2021-03-21 22:13:19 +0700

    test

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,6 @@
 <?php
-$koneksi = mysqli_connect("localhost","root","","konteskicau");
-if(mysqli_connect_error()){
-    echo "Koneksi Gagal".mysqli_connect_error();
+$koneksi = mysqli_connect("localhost", "root", "", "konteskicau");
+if (mysqli_connect_error()) {
+    echo "Koneksi Gagal" . mysqli_connect_error();
 }
+//test
